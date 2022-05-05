32ef1bcfa7902d7de4b1ae944df3a953297faa9e
Christos <christos.papadopoulos@student.hepl.be>
2022-05-05 13:04:41 +0200

    Ajout de l'analyseur Ajout du AND et OR

--- analyseur.py
+++ analyseur.py
@@ -1,17 +1,10 @@
 def Prog():
     SymboleSuivant(0)
-    if SymboleCourant(6) == "start>":
-        SymboleSuivant(6)
-        codeCible.append("void main()")
-        codeCible.append("{")
-        codeCible.append("\t_asm")
-        codeCible.append("\t{")
+    if SymboleCourant(5) == "start":
+        SymboleSuivant(5)
         ExprO()
-        if SymboleCourant(5) == "<stop":
-            SymboleSuivant(5)
-            codeCible.append("\t\tpop eax")
-            codeCible.append("\t}")
-            codeCible.append("}")
+        if SymboleCourant(4) == "stop":
+            SymboleSuivant(4)
             return True
         else:
             return False
@@ -24,19 +17,19 @@ def ExprO():
     if SymboleCourant(1) == '|':
         SymboleSuivant(1)
         ExprA()
-        codeCible.append("\t\tpop ebx")
-        codeCible.append("\t\tpop eax")
-        codeCible.append("\t\tor eax, ebx")
-        codeCible.append("\t\tpush eax")
+        nb2 = popResultat()
+        nb1 = popResultat()
+        resultat.append(nb1 | nb2)
 def ExprA():
     ExprPM()
     if SymboleCourant(1) == '&':
         SymboleSuivant(1)
         ExprPM()
-        codeCible.append("\t\tpop ebx")
-        codeCible.append("\t\tpop eax")
-        codeCible.append("\t\tand eax, ebx")
-        codeCible.append("\t\tpush eax")
+        nb2 = popResultat()
+        nb1 = popResultat()
+        resultat.append(nb1 & nb2)
+
+
 
 def ExprPM():
     ExprFD()
@@ -44,50 +37,41 @@ def ExprPM():
         if SymboleCourant(1) == '+':
             SymboleSuivant(1)
             ExprFD()
-            codeCible.append("\t\tpop ebx")
-            codeCible.append("\t\tpop eax")
-            codeCible.append("\t\tadd eax, ebx")
-            codeCible.append("\t\tpush eax")
+            nb2 = popResultat()
+            nb1 = popResultat()
+            resultat.append(nb1 + nb2)
         elif SymboleCourant(1) == '-':
             SymboleSuivant(1)
             ExprFD()
-            codeCible.append("\t\tpop ebx")
-            codeCible.append("\t\tpop eax")
-            codeCible.append("\t\tsub eax, ebx")
-            codeCible.append("\t\tpush eax")
+            nb2 = popResultat()
+            nb1 = popResultat()
+            resultat.append(nb1 - nb2)
 
 # ----------------------------------------
-
 def ExprFD():
     Facteur()
     while SymboleCourant(1) in "*/%":
         if SymboleCourant(1) == '*':
             SymboleSuivant(1)
             Facteur()
-            codeCible.append("\t\tpop ebx")
-            codeCible.append("\t\tpop eax")
-            codeCible.append("\t\timul eax, ebx")
-            codeCible.append("\t\tpush eax")
+            nb2 = popResultat()
+            nb1 = popResultat()
+            resultat.append(nb1 * nb2)
         elif SymboleCourant(1) == '/':
             SymboleSuivant(1)
             Facteur()
-            codeCible.append("\t\tpop ebx")
-            codeCible.append("\t\tpop eax")
-            codeCible.append("\t\tcdq")
-            codeCible.append("\t\tidiv ebx")
-            codeCible.append("\t\tpush eax")
-        elif SymboleCourant(1) == '%':
+            nb2 = popResultat()
+            nb1 = popResultat()
+            resultat.append(int(nb1 / nb2))
+        elif SymboleCourant(1) =='%':
             SymboleSuivant(1)
             Facteur()
-            codeCible.append("\t\tpop ebx")
-            codeCible.append("\t\tpop eax")
-            codeCible.append("\t\tcdq")
-            codeCible.append("\t\tidiv ebx")
-            codeCible.append("\t\tpush edx")
-
+            nb2 = popResultat()
+            nb1 = popResultat()
+            resultat.append(int(nb1 % nb2))
 # ----------------------------------------
 def Nombre():
-    nb =""
+    nb = ""
     if Chiffre() == True:
         nb = ""
         nb = ord(SymboleCourant(1)) - 0x30
@@ -95,7 +79,6 @@ def Nombre():
         while Chiffre() == True:
             nb = nb * 10 + ord(SymboleCourant(1)) - 0x30
             SymboleSuivant(1)
-
     if SymboleCourant(1) == "b" or SymboleCourant(1) == "B":
         nb = ""
         SymboleSuivant(1)
@@ -111,8 +94,9 @@ def Nombre():
             nb += SymboleCourant(1)
             SymboleSuivant(1)
         nb = int(nb, 16)
-    codeCible.append("\t\tpush dword ptr " + str(nb))
+    resultat.append(nb)
 
+# ---------------------------------------
 def Facteur():
     if SymboleCourant(1) == '(':
         SymboleSuivant(1)
@@ -121,8 +105,8 @@ def Facteur():
             SymboleSuivant(1)
     else:
         Nombre()
-
 # ----------------------------------------
+
 
 def Binaire():
     if SymboleCourant(1) in "01":
@@ -135,37 +119,35 @@ def Hexa():
         return True
     else:
         return False
-
+# ----------------------------------------
 def Chiffre():
     if SymboleCourant(1) in "0123456789":
         return True
-    else:
+    else :
         return False
 
 # ----------------------------------------
-
-
-def SymboleCourant(n):
-    return programme[posCourante:posCourante + n]
+def popResultat():
+    if len(resultat) > 0:
+        return resultat.pop()
+    else:
+        print("Valeur manquante dans l'expression !")
+        exit()
 
 # ----------------------------------------
-
+def SymboleCourant(n):
+    return programme[posCourante:posCourante + n]
+# ---------------------------------------
 def SymboleSuivant(n):
     global posCourante
     posCourante = posCourante + n
     while posCourante < len(programme) and programme[posCourante] == ' ':
         posCourante = posCourante + 1
-
 # ----------------------------------------
-
-programme = ("start> 512 * (144 + 0b11) | 0x12 - 541 & 0xff <stop")
-codeCible = []
+programme = ("start" "512 * (144 + 0b11) | 0x12 - 541 & 0xff" "stop")
+resultat = []
 posCourante = 0
-
 if Prog() == True:
-    for c in codeCible:
-        print(c)
-    print("\nCompilation terminée avec succès!")
+    print("Le résultat de l'expression est :", popResultat())
 else:
-    print("Erreur de compilation : le caractere", SymboleCourant(1),
-          "à la " + str(posCourante + 1) + "e position est invalide!")
+    print("Erreur dans le code source à partir de la " + str(posCourante + 1) + "e position !")
